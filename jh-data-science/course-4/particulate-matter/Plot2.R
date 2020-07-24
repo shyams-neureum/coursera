fba25f2c819cc817eab6cc5e4b4a414795319284
Shyam Sundar <shyam.sundar@neureum.tech>
2020-07-23 19:35:39 -0700

    First Version of Plot2.R

---
+++ jh-data-science/course-4/particulate-matter/Plot2.R
@@ -0,0 +1,127 @@
+##
+## Function: Plot2
+##
+## Description: This function answers question #2: "Have total emissions from 
+## PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 
+## to 2008? Use the base plotting system to make a plot answering this question. 
+##
+## Returns: None
+##
+## Parameters: None
+##
+Plot2 <- function()
+{
+    # Open the files and load the data
+    scc_file_path <- file.path(
+                        "exdata_data_NEI_data", 
+                        "Source_Classification_Code.rds")
+    
+    sum_file_path <- file.path(
+                        "exdata_data_NEI_data", 
+                        "summarySCC_PM25.rds")
+    
+    scc_data <- readRDS(scc_file_path)
+    summary_data <- readRDS(sum_file_path)
+    
+    # Filter the dataset for just Baltimore City, Maryland
+    filter_data <- subset(summary_data, summary_data$fips=="24510")
+    
+    # We will be calculating the total sum of emissions grouped by year. For 
+    # good effect, we will also calculate mean, max and median grouped by year
+    # draw one plot with all 4.
+    sums <- filter_data %>% 
+            group_by(year) %>% 
+            summarize(sum(Emissions,na.rm=TRUE))
+    colnames(sums) <- c("Year", "Sum")
+    
+    maxes <- filter_data %>% 
+             group_by(year) %>% 
+             summarize(max(Emissions,na.rm=TRUE))
+    colnames(maxes) <- c("Year", "Max")
+    
+    means <- filter_data %>% 
+             group_by(year) %>% 
+             summarize(mean(Emissions,na.rm=TRUE))
+    colnames(means) <- c("Year", "Mean")
+    
+    medians <- filter_data %>% 
+               group_by(year) %>% 
+               summarize(median(Emissions,na.rm=TRUE))
+    colnames(medians) <- c("Year", "Median")
+    
+    png("Plot2.png", width=1000, height=500)
+    
+    par(mfrow=c(2,2))
+    
+    # Plot for Total Emissions
+    max_sums <- max(sums$Sum)
+    marks <- c(0, max_sums/2, max_sums)
+    marks_str <- c(0,
+                   sprintf("%.2f", max_sums/2),
+                   sprintf("%.2f", max_sums))
+    
+    barplot(
+        sums$Sum~sums$Year, 
+        main="Total Emissions by Year (Baltimore)",
+        xlab="Year",
+        ylab="Total Emissions",
+        col=c("red","blue","green","yellow"),
+        yaxt="n",
+        ylim=c(0, max_sums)
+    )
+    
+    axis(2, at=marks, labels=marks_str)
+
+    # Plot for Average Emissions 
+    max_means <- max(means$Mean)
+    marks <- c(0, max_means/2, max_means)
+    marks_str <- c("0", sprintf("%.2f", max_means/2), sprintf("%.2f", max_means))
+    
+    barplot(
+        means$Mean~means$Year, 
+        main="Average Emissions by Year (Baltimore)",
+        xlab="Year",
+        ylab="Average Emissions",
+        col=c("red","blue","green","yellow"),
+        yaxt="n",
+        ylim=c(0, max_means)
+    )
+    
+    axis(2, at=marks, labels=marks_str)
+    
+    # Plot for Median Emissions
+    max_median <- max(medians$Median)
+    marks <- c(0, max_median/2, max_median)
+    marks_str <- c("0", sprintf("%.2f", max_median/2), sprintf("%.2f", max_median))
+    
+    barplot(
+        medians$Median~medians$Year, 
+        main="Median Emissions by Year (Baltimore)",
+        xlab="Year",
+        ylab="Median Emissions",
+        col=c("red","blue","green","yellow"),
+        yaxt="n",
+        ylim=c(0, max_median)
+    )
+    
+    axis(2, at=marks, labels=marks_str)
+    
+    # Plot for Maximum Emissions
+    max_maxes <- max(maxes$Max)
+    marks <- c(0, max_maxes/2, max_maxes)
+    marks_str <- c("0", sprintf("%.2f", max_maxes/2), sprintf("%.2f", max_maxes))
+    
+    barplot(
+        maxes$Max~maxes$Year, 
+        main="Maximum Emissions by Year (Baltimore)",
+        xlab="Year",
+        ylab="Maximum Emissions",
+        col=c("red","blue","green","yellow"),
+        yaxt="n",
+        ylim=c(0, max_maxes)
+    )
+    
+    axis(2, at=marks, labels=marks_str)
+    
+    dev.off()
+}
